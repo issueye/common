6adb06ed0cec3d46e0a4acc4d6bc3d1c65bd5c95
issueye <issueye@yeah.net>
2024-08-22 18:11:01 +0800

    Update index.go

--- tdb/index.go
+++ tdb/index.go
@@ -1,28 +1,18 @@
 package tdb
 
 import (
-	"github.com/dgraph-io/badger/v4"
 	badgerhold "github.com/timshannon/badgerhold/v4"
 )
 
-type TDB struct {
-	*badgerhold.Store
-}
+func NewTDB(path string) (*badgerhold.Store, error) {
+	options := badgerhold.DefaultOptions
+	options.Dir = path
+	options.ValueDir = path
 
-func NewTDB(path string) (*TDB, error) {
-	opts := badger.DefaultOptions(path)
-	store, err := badgerhold.Open(badgerhold.Options{
-		Options: opts,
-	})
+	store, err := badgerhold.Open(options)
 	if err != nil {
 		return nil, err
 	}
 
-	return &TDB{
-		Store: store,
-	}, nil
-}
-
-func (tdb *TDB) Close() error {
-	return tdb.Store.Close()
+	return store, nil
 }
